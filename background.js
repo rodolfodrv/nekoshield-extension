ad5bf8e8b27f533fdf5c7ed99a569d826b309946
rodolfodrv <rodolfodelrio@gmail.com>
2026-04-08 09:29:52 -0600

    Create background.js

---
+++ background.js
@@ -0,0 +1,39 @@
+const API_URL = 'https://nekoshield-server.onrender.com';
+
+chrome.runtime.onInstalled.addListener(function() {
+  console.log('NekoShield extension installed');
+  chrome.storage.local.set({ scannedUrls: {} });
+});
+
+async function analyzeUrl(url) {
+  try {
+    var scannedUrls = await chrome.storage.local.get('scannedUrls');
+    var cache = scannedUrls.scannedUrls || {};
+    
+    if (cache[url]) return cache[url];
+
+    var response = await fetch(API_URL + '/analyze', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ url: url })
+    });
+
+    var data = await response.json();
+    
+    cache[url] = data;
+    chrome.storage.local.set({ scannedUrls: cache });
+    
+    return data;
+  } catch(e) {
+    return null;
+  }
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === 'analyzeUrl') {
+    analyzeUrl(request.url).then(function(result) {
+      sendResponse({ result: result });
+    });
+    return true;
+  }
+});
